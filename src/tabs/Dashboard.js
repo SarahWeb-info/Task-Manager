ba0ea0a538b8ec15e1394d3f0258193399f05bf7
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-11-19 11:53:26 +0500

    all filters working

--- src/tabs/Dashboard.js
+++ src/tabs/Dashboard.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import TaskCards from '../components/TaskCards';
 import '../css/dashboard.css';
+import {BsSearch , BsBellFill} from "react-icons/bs";
 
 export default function Dashboard() {
+  const [showStringInput, setShowStringInput] = useState(false);
+
     let dataObj = JSON.parse(localStorage.getItem('tasks')) || {};
     //converting data object in array of keys and values
     const data = Object.entries(dataObj);
+    data.sort((a, b) => new Date(b[1].Time) - new Date(a[1].Time)); //descending
+
     let dataPresent = false;
     if (data.length>0) {
       dataPresent = true;
@@ -47,11 +52,102 @@ export default function Dashboard() {
       };
     };
   
+    const [ showArr , setShowArr ] = useState(data);
+
+    const arrayFilter=(input)=>{
+      let arr = [];
+      let searchFor = "to do";
+
+      if (input === 3) {
+        searchFor = "missed";
+      }else if (input === 2) {
+        searchFor = "done";
+      }else if (input === 1) {
+        searchFor = "to do";
+      }else if (input === 0) {
+        arr = data;
+      }
+
+      for (let i in data) {
+        let sortTask = data[i]; //the whole tasks
+        
+        if (data[i][1].Status === searchFor) {
+          arr.push(sortTask);
+        }  
+      }
+      
+      setShowArr(arr);
+    }
+    
+    const strFilter =(inputValue)=>{
+      let arr = [];
+      let searchFor = inputValue.toLowerCase();
+      
+      for (let i in data) {
+        let sortTask = data[i]; //the whole tasks
+        
+        let searchIn = data[i][1].Title + data[i][1].Task + data[i][1].Category + data[i][1].Collaborates;
+        searchIn = searchIn.toLowerCase();
+
+        // console.log(searchIn);
+        if (searchIn.includes(searchFor) ) {
+          arr.push(sortTask);
+          console.log("got it ");
+        }  
+      }
+      setShowArr(arr);
+    }
+
+    const dateFilter =(inputValue)=>{
+      let arr = [];
+      let inputDate = dateFormat(inputValue);
+
+      for (let i in data) {
+        let sortTask = data[i]; //the whole tasks
+        let taskDate = dateFormat(data[i][1].Time);
+        // here we have to compare the dates (not time) -taskDate and inputDate
+        if( taskDate.year === inputDate.year && taskDate.month === inputDate.month && taskDate.date === inputDate.date ){
+          arr.push(sortTask);
+          console.log("got it ");
+        }
+      }
+      setShowArr(arr);
+    }
+
     let prevDate;
     return (
       <>
+      <div className = 'inlineFlexStatus'>
+        <div className = 'statusChild2'>
+          
+          <button onClick={()=>arrayFilter(0)} className='noBtn'>ALL</button>
+          <button onClick={()=>arrayFilter(3)} className='noBtn'>Missed</button>
+          <button onClick={()=>arrayFilter(2)} className='noBtn'>Done's</button>
+          <button onClick={()=>arrayFilter(1)} className='noBtn'>To Do's</button>
+        </div>
+        <div className = 'statusChild2' >
+          <input
+          type="text"
+          id="inputSearch"
+          onInput = {(e) => strFilter(e.target.value)}
+          style={{color : 'black'}}
+        />
+  
+          <label htmlFor = "inputSearch">
+            <BsSearch />
+          </label>
+              
+          <input
+          type="date"
+          onInput = {(e) => dateFilter(e.target.value)}
+          style={{color : 'black'}}
+        />
+
+        </div> 
+      </div> 
+
       {
-        dataPresent && data.map(([key, task]) => {
+        dataPresent && showArr.map(([key, task]) => {
           const formatedTime = dateFormat(task.Time);
           const showGrayLine = formatedTime.date !== prevDate;
           let taskTime = formatedTime.hours +":" + formatedTime.minutes ;
